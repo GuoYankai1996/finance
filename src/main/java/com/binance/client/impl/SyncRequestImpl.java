f8a913152ab54f4bbe2acf10df3d09ac8ff92040
zhouzhipeng <admin@zhouzhipeng.com>
2020-04-20 16:45:46 +0800

    update

--- src/main/java/com/binance/client/impl/SyncRequestImpl.java
+++ src/main/java/com/binance/client/impl/SyncRequestImpl.java
@@ -94,10 +94,10 @@ public class SyncRequestImpl implements SyncRequestClient {
     @Override
     public Order postOrder(String symbol, OrderSide side, PositionSide positionSide, OrderType orderType,
             TimeInForce timeInForce, String quantity, String price, String reduceOnly,
-            String newClientOrderId, String stopPrice, WorkingType workingType) {
+            String newClientOrderId, String stopPrice, WorkingType workingType, NewOrderRespType newOrderRespType) {
         return RestApiInvoker.callSync(requestImpl.postOrder(symbol, side, positionSide, orderType,
                 timeInForce, quantity, price, reduceOnly, 
-                newClientOrderId, stopPrice, workingType));
+                newClientOrderId, stopPrice, workingType,newOrderRespType));
     }
     
     @Override
