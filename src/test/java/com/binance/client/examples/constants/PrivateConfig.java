48dffefe0452d9092207e6bbd686c52a3b9c7a0f
wangwanlu <wanlu.wang@binance.com>
2020-04-24 21:57:17 +0800

    minor

--- src/test/java/com/binance/client/examples/constants/PrivateConfig.java
+++ src/test/java/com/binance/client/examples/constants/PrivateConfig.java
@@ -2,7 +2,7 @@ package com.binance.client.examples.constants;
 
 public class PrivateConfig {
 
-    public static final String API_KEY = "DO2VJ5LrPVjovReHPPFIO59GF3ZFviKdtz0bt5sLHwed0R8IstZU92OBZ9Ows2sL";
-    public static final String SECRET_KEY = "h1jn2USxSyFICIaBj0xx1qcFALv3jDbEYqa4uSteHHXvtoqNlrfq3l4VJH8TnyiL";
+    public static final String API_KEY = "XXXXXX";
+    public static final String SECRET_KEY = "XXXXXX";
 
 }
